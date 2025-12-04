eadff96fd5da7c5aad2b27a7600bddf8fcb356a4
tmshkr <5200096+tmshkr@users.noreply.github.com>
2025-12-03 18:32:29 -0800

    add MainActivity lateinit vars

--- app/src/main/java/com/example/habittracker/MainActivity.kt
+++ app/src/main/java/com/example/habittracker/MainActivity.kt
@@ -24,8 +24,16 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.example.habittracker.ui.theme.HabitTrackerTheme
+import android.content.SharedPreferences
+import com.example.habittracker.data.AppRepository
+import com.example.habittracker.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var repo: AppRepository
+
+    private lateinit var sharedPref: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
