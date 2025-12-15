eafff2829e8a82265a1a0777c1981a57240f112d
scantro <jacdep798@gmail.com>
2025-12-15 13:54:22 -0800

    Added AppCompatActivity and 3 declared variables

--- app/src/main/java/com/example/habittracker/HabitLogActivity.kt
+++ app/src/main/java/com/example/habittracker/HabitLogActivity.kt
@@ -1,5 +1,12 @@
 package com.example.habittracker
+import androidx.appcompat.app.AppCompatActivity
 
-class HabitLogActivity {
+class HabitLogActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_HABIT_ID = "extra_habit_id"
+        const val EXTRA_USER_ID = "extra_user_id"
+        const val EXTRA_DATE_MILLIS = "extra_date_millis"
+    }
 
 }
